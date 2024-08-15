f875e45d560b3393474bb6780b57fd2e288a77aa
travjsmit <inivid@icloud.com>
2024-08-15 15:12:53 -0700

    Add project files.

---
+++ AdjacencyList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class AdjacencyList
+    {
+        public Dictionary<int, LinkedList<int>> list = new();
+
+        public AdjacencyList(int verticesSize)
+        {
+
+        }
+    }
+}
